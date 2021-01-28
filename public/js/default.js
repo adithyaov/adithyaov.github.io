61cdaf6c4bd27c59323a3210d398e48d7a93fd9f
adithyaov <adi.obilisetty@gmail.com>
2021-01-28 10:48:26 +0530

    rebuilding site Thu Jan 28 10:48:26 IST 2021

--- public/js/default.js
+++
@@ -1,7 +0,0 @@
-var msg = "";
-
-msg = "Changing inline-css for language-haskell"
-console.log("BEGIN: ".concat(msg));
-var elem = document.getElementByClassName('language-haskell');
-elem.style.color = "#fff";
-console.log("END: ".concat(msg));
